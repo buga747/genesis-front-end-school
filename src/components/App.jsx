8e09ce6fb4290363be621b7ef7ba92edef8de579
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-03-13 22:30:24 +0200

    starrt of the project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+
+const Courses = lazy(() => import('pages/Courses'));
+const Course = lazy(() => import('pages/Course'));
+const SharedLayout = lazy(() => import('./SharedLayout'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Courses />} />
+            <Route path="/course/:name" element={<Course />} />
+          </Route>
+        </Routes>
+        <GlobalStyle />
+      </Suspense>
+    </>
   );
 };
